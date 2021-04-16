b1e6be0e549062b2d81eb24e8af3fa852af9f466
ev <zhuochen1029@gmail.com>
2021-04-16 22:16:11 +1000

    Add missing code

--- chp03/Merging polygons using a common attribute/code/merge_poly.sql
+++ chp03/Merging polygons using a common attribute/code/merge_poly.sql
@@ -1,2 +1,6 @@
 select county, fips, state_fips from chp03.counties
 order by county;
+
+CREATE TABLE chp03.states_from_counties
+         AS SELECT ST_Multi(ST_Union(the_geom)) as the_geom, state_fips
+         FROM chp03.counties GROUP BY state_fips;
